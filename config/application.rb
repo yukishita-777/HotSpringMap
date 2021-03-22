9d8be6e5e876bf4a2ea030bd1d2c67f3338f196b
雪下　佳太 <yukishita@mba.local>
2021-03-22 22:38:08 +0900

    rubocop,rspecとcircleciの連携

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,13 @@ module HotSpringMap
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.generators do |g|
+      g.test_framework :rspec, 
+            view_specs: false, 
+            helper_specs: false, 
+            controller_specs: false, 
+            routing_specs: false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
