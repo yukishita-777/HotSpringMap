7ba8dd200878360c4ec14da735fda6f4627e0272
雪下　佳太 <yukishita@mba.local>
2021-04-29 21:36:16 +0900

    場所検索に対応、ドラッグした位置でもスポットを表示

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,5 +18,5 @@ import "src/css/index.scss";
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
